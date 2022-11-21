42e6f3c6976bc12e30ad84f0d461f286a980c6a6
CTurkowsky <91448665+CTurkowsky@users.noreply.github.com>
2022-11-21 00:17:54 -0500

    Config Produccion

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 import { createPool } from 'mysql2/promise';
+import {
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    DB_PORT,
+ } from './config.js'
 
 export const pool = createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Turkowsky11',
-    database: 'inventario'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 
 });
 
